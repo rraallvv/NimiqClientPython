36e1ab21427f274a6c36bf89cc213f49e65dbef4
Developer <>
2020-07-24 01:30:05 -0400

    add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='nimiqclient',
     version='0.0.1',
     description='A python client for the Nimiq JSON-RPC API',
+    long_description=long_description,
     url='http://github.com/rraallvv/NimiqClientPython',
     author='Nimiq Community',
     author_email='info@nimiq.com',
